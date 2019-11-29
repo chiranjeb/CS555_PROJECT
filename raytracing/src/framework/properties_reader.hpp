04621d9cb7ae37675cc9034ce22f3ee4c165441c
chiranjeb <chiranjeb.mondal@gmail.com>
2019-11-29 11:35:49 -0800

    - Properties bug fix to remove carriage return.

--- raytracing/src/framework/properties_reader.hpp
+++ raytracing/src/framework/properties_reader.hpp
@@ -9,32 +9,33 @@
 class PropertiesReader
 {
 public:
-   PropertiesReader(std::string &filename)
-   {
-      std::ifstream file(filename);
-      std::string line;
-      while (std::getline(file, line))
-      {
-         /// Let's filter out the lines with the comments.
-         if ( line.find("=") != std::string::npos)
-         {
-         std::stringstream linestream(line);
-         std::string token1, token2;
-         std::getline(linestream, token1, '=');
-         std::getline(linestream, token2, '=');
-         m_PropertiesVsValue[token1] = token2;
-         }
-      }
-   }
+    PropertiesReader(std::string& filename)
+    {
+        std::ifstream file(filename);
+        std::string line;
+        while (std::getline(file, line))
+        {
+            /// Let's filter out the lines with the comments.
+            if (line.find("=") != std::string::npos)
+            {
+                line.erase(std::remove(line.begin(), line.end(), '\r'), line.end()); 
+                std::stringstream linestream(line);
+                std::string token1, token2;
+                std::getline(linestream, token1, '=');
+                std::getline(linestream, token2, '=');
+                m_PropertiesVsValue[token1] = token2;
+            }
+        }
+    }
 
-   std::string operator[](std::string key)
-   {
-      return m_PropertiesVsValue[key];
-   }
+    std::string operator[](std::string key)
+    {
+        return m_PropertiesVsValue[key];
+    }
 
 
 protected:
-   std::map<std::string, std::string> m_PropertiesVsValue;
+    std::map<std::string, std::string> m_PropertiesVsValue;
 };
 
 
