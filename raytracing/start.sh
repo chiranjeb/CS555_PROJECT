4a4e7099ea4325b8cfc0451727a6b5ce7175197e
dwhite54 <david.white.679@gmail.com>
2019-11-30 15:33:01 -0700

    Put master_properties.txt back...

--- raytracing/start.sh
+++ raytracing/start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 projHome=/s/red/b/nobackup/data/portable/CS555_PROJECT/raytracing
-me=$(hostname)
+totalPanes=0
 tmux new -s cs555proj -d
-echo "Starting client@$(hostname)"
-tmux send-keys -t cs555proj "ssh -t $(hostname).cs.colostate.edu 'cd $projHome; ./build/client properties/master_properties.txt properties/client_properties.txt random 10 10 10'" Enter
 while read machine
 do
+		echo "Starting worker@${machine}"
+    tmux send-keys -t cs555proj "ssh -t ${machine}.cs.colostate.edu 'cd $projHome; \
+      ./build/worker properties/master_properties.txt properties/worker_properties.txt'" Enter
+		totalPanes=$((totalPanes+1))
 		if [ $totalPanes -eq "8" ]; then
 		  tmux new-window -t cs555proj
 		  totalPanes=0
@@ -13,12 +15,15 @@ do
 		  tmux split-window -t cs555proj
 		  tmux select-layout -t cs555proj even-vertical
 		fi
-		echo "Starting worker@${machine}"
-    tmux send-keys -t cs555proj "ssh -t ${machine}.cs.colostate.edu 'cd $projHome; ./build/worker properties/master_properties.txt properties/worker_properties.txt'" Enter
-		totalPanes=$((totalPanes+1))
 done < $1
-tmux split-window -t cs555proj
+tmux send-keys -t cs555proj "exit" Enter
 tmux select-layout -t cs555proj even-vertical
+tmux new-window -t cs555proj
 echo "Starting master@$(hostname)"
-tmux send-keys -t cs555proj "ssh -t $(hostname).cs.colostate.edu 'cd $projHome; ./build/master properties/master_properties.txt'" Enter
+tmux send-keys -t cs555proj "ssh -t $(hostname).cs.colostate.edu 'cd $projHome; \
+  ./build/master properties/master_properties.txt'" Enter
+tmux split-window -t cs555proj
+echo "Starting client@$(hostname)"
+tmux send-keys -t cs555proj "ssh -t $(hostname).cs.colostate.edu 'cd $projHome; \
+  ./build/client properties/master_properties.txt properties/client_properties.txt random 1000 1000 1000'" Enter
 tmux attach -t cs555proj
