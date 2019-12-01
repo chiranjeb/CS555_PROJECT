f8cced8d170f9c004b314ddb00ba2162b7ed4d23
dwhite54 <david.white.679@gmail.com>
2019-12-01 14:17:35 -0700

    fix script formatting

--- raytracing/scripts/start.sh
+++ raytracing/scripts/start.sh
@@ -2,24 +2,23 @@
 totalPanes=0
 i=0
 tmux new -s cs555proj -d
-while read machine
-do
-		echo "Starting worker@${machine}"
-		tmux send-keys -t cs555proj "ssh -t ${machine}.cs.colostate.edu 'cd $(pwd); \
+while read machine; do
+  echo "Starting worker@${machine}"
+  tmux send-keys -t cs555proj "ssh -t ${machine}.cs.colostate.edu 'cd $(pwd); \
 			./build/worker properties/master_properties.txt properties/worker_properties.txt'" Enter
-		totalPanes=$((totalPanes+1))
-		i=$((i+1))
-		if [ $totalPanes -eq "8" ]; then
-		  tmux new-window -t cs555proj
-		  totalPanes=0
-		else
-		  tmux split-window -t cs555proj
-		  tmux select-layout -t cs555proj even-vertical
-		fi
-		if [ $i -eq $1 ]; then
-		  break
-		fi
-done < "scripts/275machines.txt"
+  totalPanes=$((totalPanes + 1))
+  i=$((i + 1))
+  if [ $totalPanes -eq "8" ]; then
+    tmux new-window -t cs555proj
+    totalPanes=0
+  else
+    tmux split-window -t cs555proj
+    tmux select-layout -t cs555proj even-vertical
+  fi
+  if [ $i -eq $1 ]; then
+    break
+  fi
+done <"scripts/275machines.txt"
 tmux send-keys -t cs555proj "exit" Enter
 tmux select-layout -t cs555proj even-vertical
 tmux new-window -t cs555proj
