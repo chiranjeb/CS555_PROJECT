04aa233e993618d807dd322faebdf9d25c4e162b
dwhite54 <david.white.679@gmail.com>
2019-12-01 19:16:47 -0700

    remove scene parameter from script

--- raytracing/scripts/start.sh
+++ raytracing/scripts/start.sh
@@ -18,7 +18,7 @@ while read machine; do
 			./build/worker properties/master_properties.txt properties/worker_properties.txt'" Enter
   totalPanes=$((totalPanes + 1))
   i=$((i + 1))
-  if [ $i -eq $2 ]; then
+  if [ $i -eq $1 ]; then
     break
   fi
 done < "scripts/275machines.txt"
@@ -27,6 +27,6 @@ tmux split-window -t cs555proj
 echo "Starting client@$(hostname)"
 sleep 10
 tmux send-keys -t cs555proj "ssh -t $(hostname).cs.colostate.edu 'cd $(pwd); \
-  ./build/client properties/master_properties.txt properties/client_properties.txt $2 $3 $4 $5'" Enter
+  ./build/client properties/master_properties.txt properties/client_properties.txt random $2 $3 $4'" Enter
 tmux select-layout -t cs555proj even-vertical
 tmux attach -t cs555proj
