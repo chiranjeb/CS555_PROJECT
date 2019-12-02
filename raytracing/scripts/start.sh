e572d4ad72f8c55702cb86ccfcaa6326444b77a7
dwhite54 <david.white.679@gmail.com>
2019-12-01 18:26:41 -0700

    Fix script again...

--- raytracing/scripts/start.sh
+++ raytracing/scripts/start.sh
@@ -18,14 +18,15 @@ while read machine; do
 			./build/worker properties/master_properties.txt properties/worker_properties.txt'" Enter
   totalPanes=$((totalPanes + 1))
   i=$((i + 1))
-  if [ $i -eq $1 ]; then
+  if [ $i -eq $2 ]; then
     break
   fi
 done < "scripts/275machines.txt"
 tmux select-layout -t cs555proj even-vertical
 tmux split-window -t cs555proj
 echo "Starting client@$(hostname)"
+sleep 10
 tmux send-keys -t cs555proj "ssh -t $(hostname).cs.colostate.edu 'cd $(pwd); \
-  ./build/client properties/master_properties.txt properties/client_properties.txt random $2 $3 $4'"
+  ./build/client properties/master_properties.txt properties/client_properties.txt $2 $3 $4 $5'" Enter
 tmux select-layout -t cs555proj even-vertical
 tmux attach -t cs555proj
