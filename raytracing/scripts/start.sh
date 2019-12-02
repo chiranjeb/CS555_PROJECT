fcc5f5e08f22ae43461c24aa6438fcdc4dd22d1c
dwhite54 <david.white.679@gmail.com>
2019-12-01 17:32:44 -0700

    Fix start script again

--- raytracing/scripts/start.sh
+++ raytracing/scripts/start.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-totalPanes=0
+totalPanes=1
 i=0
 tmux new -s cs555proj -d
+echo "Starting master@$(hostname)"
+tmux send-keys -t cs555proj "ssh -t $(hostname).cs.colostate.edu 'cd $(pwd); \
+  ./cmake-build-debug/master properties/master_properties.txt'" Enter
 while read machine; do
-  echo "Starting worker@${machine}"
-  tmux send-keys -t cs555proj "ssh -t ${machine}.cs.colostate.edu 'cd $(pwd); \
-			./build/worker properties/master_properties.txt properties/worker_properties.txt'" Enter
-  totalPanes=$((totalPanes + 1))
-  i=$((i + 1))
   if [ $totalPanes -eq "8" ]; then
     tmux new-window -t cs555proj
     totalPanes=0
@@ -15,19 +13,19 @@ while read machine; do
     tmux split-window -t cs555proj
     tmux select-layout -t cs555proj even-vertical
   fi
+  echo "Starting worker@${machine}"
+  tmux send-keys -t cs555proj "ssh -t ${machine}.cs.colostate.edu 'cd $(pwd); \
+			./cmake-build-debug/worker properties/master_properties.txt properties/worker_properties.txt'" Enter
+  totalPanes=$((totalPanes + 1))
+  i=$((i + 1))
   if [ $i -eq $1 ]; then
     break
   fi
-done <"scripts/275machines.txt"
-tmux send-keys -t cs555proj "exit" Enter
+done < "scripts/275machines.txt"
 tmux select-layout -t cs555proj even-vertical
-tmux new-window -t cs555proj
-echo "Starting master@$(hostname)"
-tmux send-keys -t cs555proj "ssh -t $(hostname).cs.colostate.edu 'cd $(pwd); \
-  ./build/master properties/master_properties.txt'" Enter
 tmux split-window -t cs555proj
 echo "Starting client@$(hostname)"
-sleep 2
 tmux send-keys -t cs555proj "ssh -t $(hostname).cs.colostate.edu 'cd $(pwd); \
-  ./build/client properties/master_properties.txt properties/client_properties.txt random $2 $3 $4'" Enter
+  ./cmake-build-debug/client properties/master_properties.txt properties/client_properties.txt random $2 $3 $4'"
+tmux select-layout -t cs555proj even-vertical
 tmux attach -t cs555proj
