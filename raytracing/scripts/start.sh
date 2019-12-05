9891cf6d02cc44d19e7f1783a67a5982c54ce6fc
dwhite54 <david.white.679@gmail.com>
2019-12-04 17:06:04 -0700

    fix start script with new additions

--- raytracing/scripts/start.sh
+++ raytracing/scripts/start.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 totalPanes=1
-i=0
 tmux new -s cs555proj -d
 echo "Starting master@$(hostname)"
 tmux send-keys -t cs555proj "ssh -t $(hostname).cs.colostate.edu 'cd $(pwd); \
   ./build/master properties/master_properties.txt'" Enter
 while read machine; do
-  if [ $totalPanes -eq "8" ]; then
-    tmux new-window -t cs555proj
-    totalPanes=0
+  if [ ${machine} == $(hostname) ]; then
+    echo "Skipping master"
   else
-    tmux split-window -t cs555proj
-    tmux select-layout -t cs555proj even-vertical
+    if [ $totalPanes -eq "8" ]; then
+      tmux new-window -t cs555proj
+      totalPanes=0
+    else
+      tmux split-window -t cs555proj
+      tmux select-layout -t cs555proj even-vertical
+    fi
+    echo "Starting worker@${machine}"
+    tmux send-keys -t cs555proj "ssh -t ${machine}.cs.colostate.edu 'cd $(pwd); \
+  			./build/worker properties/master_properties.txt properties/worker_properties.txt'" Enter
+    totalPanes=$((totalPanes + 1))
   fi
-  echo "Starting worker@${machine}"
-  tmux send-keys -t cs555proj "ssh -t ${machine}.cs.colostate.edu 'cd $(pwd); \
-			./build/worker properties/master_properties.txt properties/worker_properties.txt'" Enter
-  totalPanes=$((totalPanes + 1))
-  i=$((i + 1))
-  if [ $i -eq $1 ]; then
-    break
-  fi
-done < "scripts/275machines.txt"
+done < $1
 tmux select-layout -t cs555proj even-vertical
 tmux split-window -t cs555proj
 echo "Starting client@$(hostname)"
